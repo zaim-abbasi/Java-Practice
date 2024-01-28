7492333025dbeb56325871ec72d4c829c6456246
Zaim Abbasi <zaim.k.abbasi@gmail.com>
2024-01-28 13:41:05 +0500

    Initial Commit

---
+++ MyFirstProgram/src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// int num = 5;
+		// System.out.println("Number is " + num);
+
+		// boolean flag = false;
+		// if(flag == false)
+		// {
+		// System.out.println("False");
+		// }
+
+		// float a = 3.4f;
+
+		// double b = 6.6;
+
+		// String x = "Water", y = "Kool-Aid";
+		// String temp;
+
+		// temp = x;
+		// x = y;
+		// y = temp;
+
+		// System.out.println("X: " + x);
+		// System.out.println("Y: " + y);
+
+		// User Inputs
+		// Scanner scanner = new Scanner(System.in);
+
+		// System.out.print("Enter Name: ");
+		// String name = scanner.nextLine();
+
+		// System.out.print("Enter age: ");
+		// int age = scanner.nextInt();
+
+		// System.out.print("Enter your City name: ");
+		// scanner = null;
+		// scanner = new Scanner(System.in);
+		// String city = scanner.nextLine();
+
+		// System.out.println("Hello " + name);
+		// System.out.println("Your age is " + age);
+		// System.out.println("Your city is "+city);
+
+		double friends = 10;
+		friends = friends / 3;
+		System.out.println(friends);
+
+	}
+
+}
---
+++ MyFirstProgram/src/calculator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+// BASIC CALCULATOR APP
+public class calculator {
+  public static void main(String[] args) {
+    // basic calculator in java
+    int num1, num2;
+    int result = 0;
+    char operator;
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter first number: ");
+    num1 = scanner.nextInt();
+    System.out.print("Enter second number: ");
+    num2 = scanner.nextInt();
+
+    System.out.println("Enter Operator: ");
+    System.out.println("+ - * /");
+    operator = scanner.next().charAt(0);
+
+    
+    if (operator == '+') {
+      // plus operation
+      result = num1 + num2;
+    }
+    else if (operator == '-') {
+      // minus operation
+      result = num1 - num2;
+    }
+    else if (operator == '*') {
+      // multiply operator
+      result = num1 * num2;
+    }
+    else if (operator == '/') {
+      // divide operator
+      result = num1 / num2;
+    }
+    else
+    System.out.println("Invalid operator");
+
+    System.out.println("Result is " + result);
+
+  }
+}
